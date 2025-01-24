94d4e6a56eae7e81d24d7015b518bac85da468e6
mdhanush96 <m.dhanush0416@gmail.com>
2025-01-24 12:33:16 +0530

    Update Day-3.py

--- Day-3.py
+++ Day-3.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Write a Python program that takes a student's marks in three subjects as input.
-# •	If the average is greater than or equal to 90, print "Grade: A".
-# •	If the average is between 80 and 89, print "Grade: B".
-# •	If the average is between 70 and 79, print "Grade: C".
-# •	Otherwise, print "Grade: Fail".
-# 
-
-# In[1]:
-
-
 subject_1 = int(input("Enter marks for Subject 1: "))
 subject_2 = int(input("Enter marks for Subject 2: "))
 subject_3 = int(input("Enter marks for Subject 3: "))
