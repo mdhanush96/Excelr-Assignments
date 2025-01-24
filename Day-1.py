2d62218caa4d73c22b2898d00c15d954c2cd451b
mdhanush96 <m.dhanush0416@gmail.com>
2025-01-24 12:31:03 +0530

    Update Day-1.py

--- Day-1.py
+++ Day-1.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # 1. Jupyter Notebook and Data Types
-# Write a program in Jupyter Notebook to declare variables of different data types (integer, float, string, and boolean). Print each variable and its type.
-# 
-
-# In[1]:
-
-
 a = 10          
 b = 3.14          
 c = "Hello, World!" 
@@ -18,7 +8,6 @@ print("String:", c, " | Type:", type(c))
 print("Boolean Value:", d, " | Type:", type(d))
 
 
-# In[ ]:
 
 
 
